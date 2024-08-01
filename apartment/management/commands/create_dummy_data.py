1d6ec815c4c8dba72f71b9f381d843f71508aa17
Boss <abdumuratovabduvali@07gmail.com>
2024-08-01 14:41:24 +0500

    fix

--- apartment/management/commands/create_dummy_data.py
+++ apartment/management/commands/create_dummy_data.py
@@ -1,29 +1,31 @@
 import random
 from django.core.management.base import BaseCommand
-from apartment.models import Apartment, ApartmentShots
+from apartment.models import Apartment, ApartmentShots, Brand, City, District
 from faker import Faker
 from django.utils import timezone
 import requests
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
-
 fake = Faker()
 
 class Command(BaseCommand):
     help = 'Generate dummy data for Apartments and their related ApartmentShots'
 
-    def handle(self, *args, **kwargs):  
-        for _ in range(10):
+    def handle(self, *args, **kwargs):
+        # Create dummy data for Brands, Cities, and Districts
+        brands = [Brand.objects.create(name=fake.company()) for _ in range(5)]
+        cities = [City.objects.create(name=fake.city()) for _ in range(3)]
+        districts = [District.objects.create(name=fake.city()) for _ in range(10)]
+
+        # Create dummy Apartments
+        for _ in range(10):  # Number of dummy apartments to create
             image_url = self.get_apartment_shot_image()
             image_name = f"apartment_{fake.word()}.jpg"
             image_path = self.save_image_from_url(image_url, image_name)
 
-        # Create dummy Apartments
-        for _ in range(10):  # Number of dummy apartments to create
             apartment = Apartment.objects.create(
-                name=fake.company(),
-                company_logo=image_path,
+                name=fake.word(),
                 company_name=fake.company(),
                 price=round(random.uniform(50.0, 500.0), 2),
                 price_per_m=round(random.uniform(5.0, 50.0), 2),
@@ -39,8 +41,11 @@ class Command(BaseCommand):
                 type=fake.word(),
                 description=fake.text(),
                 is_finish=fake.boolean(),
+                is_mortgage=fake.boolean(),
+                brand=random.choice(brands),
+                city=random.choice(cities),
+                district=random.choice(districts),
             )
-            # self.stdout.write(self.style.SUCCESS(f'Created apartment: {apartment.name}'))
 
             # Create dummy ApartmentShots for each apartment
             for shot_num in range(1, 3):
@@ -57,13 +62,11 @@ class Command(BaseCommand):
 
             self.stdout.write(self.style.SUCCESS(f'Successfully created Apartment {_ + 1} and its shots.'))
 
-        self.stdout.write(self.style.SUCCESS('Successfully created 10 dummy_data with shots.'))
-
-
+        self.stdout.write(self.style.SUCCESS('Successfully created dummy data for Apartments and ApartmentShots.'))
 
     def get_apartment_shot_image(self):
-        access_key = ''  # Replace with your Unsplash access key
-        url = f'https://api.unsplash.com/photos/random?query=product&orientation=landscape'
+        access_key = 'NNpH9MjyQNMfmuBxreAQmnSjzb2cCJk9nWCGFfd7M2M'  # Replace with your Unsplash access key
+        url = f'https://api.unsplash.com/photos/random?query=apartment&orientation=landscape'
         headers = {
             'Accept-Version': 'v1',
             'Authorization': f'Client-ID {access_key}'
@@ -74,7 +77,7 @@ class Command(BaseCommand):
             data = response.json()
             return data['urls']['regular']
         else:
-            print(response.text)
+            self.stderr.write(f'Error fetching image: {response.text}')
             return ''
 
     def save_image_from_url(self, url, image_name):
@@ -85,6 +88,7 @@ class Command(BaseCommand):
                 default_storage.save(image_path, ContentFile(response.content))
                 return image_path
             else:
+                self.stderr.write(f'Failed to fetch image from URL: {url}')
                 return None
         except Exception as e:
             self.stderr.write(f'Failed to save image from URL: {str(e)}')
